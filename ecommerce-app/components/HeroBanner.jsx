d5ec346311cc8824a914d55b8f2b7893464aa800
Taribi-Isaac <taribiforyou@gmail.com>
2023-01-03 15:38:38 +0100

    get all

--- ecommerce-app/components/HeroBanner.jsx
+++ ecommerce-app/components/HeroBanner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { urlFor } from '../lib/client';
+import Image from 'next/image';
 
 function HeroBanner({heroBanner}) {
   return (
@@ -9,9 +10,9 @@ function HeroBanner({heroBanner}) {
         <p className='beats-solo'>{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt="headphomes" className='hero-banner-image'/>
+        <image src={urlFor(heroBanner.image)} alt="headphomes" className='hero-banner-image'/>
         <div>
-          <Link href={`/product/${heroBanner.product}`}>
+          <Link href={`/product/${heroBanner.product}`} passHref>
             <button type='button'> 
             {heroBanner.buttonText}
 
