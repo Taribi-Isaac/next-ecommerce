a32a871191c81c0ca90ffa3478580c815be0d79e
Taribi-Isaac <taribiforyou@gmail.com>
2023-01-04 11:17:54 +0100

    now check

--- ecommerce-app/components/Product.jsx
+++ ecommerce-app/components/Product.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 
 import { urlFor } from '../lib/client';
 
-function Product({product: {name, image, slug, price}}) {
+const Product = ({ product: { image, name, slug, price } }) => {
   return (
-    <Link href={`/product/${slug.current}`} passHref>
-      <div className='product-card'>
-        <image src={urlFor(image && image[0])} alt="" 
-        width={250}
-        height={250}
-        className='product-image'/>
-        <p className='product-name'>{name}</p>
-        <p className='product-prce'>N{price}</p>
-
-      </div>
-     </Link>
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img 
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className="product-image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">N{price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
