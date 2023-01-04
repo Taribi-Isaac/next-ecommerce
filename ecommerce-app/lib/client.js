74fb7c216bad1eda20181db3294bc8a5336d8929
Taribi-Isaac <taribiforyou@gmail.com>
2023-01-04 12:46:19 +0100

    GET

--- ecommerce-app/lib/client.js
+++ ecommerce-app/lib/client.js
@@ -6,7 +6,7 @@ export const client = sanityClient({
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: true,
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  token: 'skdNVoSxMwmBBL2rNj8VvwZRdEY9zU3oOz4rB86sZ1SYqkO8hwAsNMJEnckrXCOrvEQJk6auR2C5KN9WbOQ0p0gwGzAiqN9jrory4NL4RRpjxq9Jq5O7XFTDaK0RE7EQpe30FbvudrXwWvCmiLSgcqGubQ9y9yJvk3a9OenpiuUZC5AIAbNg',
   ignoreBrowserTokenWarning: true
 });
 
