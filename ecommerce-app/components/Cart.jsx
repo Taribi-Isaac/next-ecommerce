4edaabb0e7474ef7ce2e4ddec6a6bc119b1ccc70
Taribi-Isaac <taribiforyou@gmail.com>
2023-01-07 10:42:27 +0100

    still debugging

--- ecommerce-app/components/Cart.jsx
+++ ecommerce-app/components/Cart.jsx
@@ -4,7 +4,7 @@ import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 
 import { TiDeleteOutline } from 'react-icons/ti';
 import toast from 'react-hot-toast';
 
-import { useStateContext } from '../context/StateContext';
+import { useStateContext } from '../context/stateContext';
 import { urlFor } from '../lib/client';
 import getStripe from '../lib/getStripe';
 
--- ecommerce-app/pages/_app.js
+++ ecommerce-app/pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/globals.css';
 import {Layout} from '../components';
-import { StateContext } from '../context/StateContext';
+import { StateContext } from '../context/stateContext';
 import { Toaster } from 'react-hot-toast';
 
 
